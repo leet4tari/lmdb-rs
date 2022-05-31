25f4eb35e5f86fce17ddd03c2089a5c12a1c8ecb
C.Lee Taylor <leet@tari.com>
2022-05-31 16:00:19 +0200

    Add target env and apple targets

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -1,3 +1,4 @@
+use std::env;
 extern crate gcc;
 
 fn main() {
@@ -11,6 +12,21 @@ fn main() {
     if target.contains("dragonfly") {
         config.flag("-DMDB_DSYNC=O_SYNC");
         config.flag("-DMDB_FDATASYNC=fsync");
+    }
+
+    if target.contains("apple-darwin") {
+        if target.contains("x86_64") {
+            config.flag("--target=x86_64-apple-macos10.12");
+        }
+
+        if target.contains("aarch64") {
+            config.flag("--target=arm64-apple-macos11");
+        }
+    }
+
+    if let Ok(target_arch) = env::var("LMDB_TARGET") {
+        //config.flag_if_supported(&format!("-march={}", target_arch));
+        config.flag(&format!("--target={}", target_arch));
     }
 
     config.compile("liblmdb.a");
